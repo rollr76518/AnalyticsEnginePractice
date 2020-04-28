89f421f93aae95e4f9d97e00c9932e32e281333e
Hanyu <rollr76518@gmail.com>
2020-04-28 22:07:38 +0800

    Initial Commit

---
+++ AnalyticsEnginePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnalyticsEnginePractice
+//
+//  Created by Ryan on 2020/4/28.
+//  Copyright © 2020 Hanyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
